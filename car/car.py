7a566e97d376696c0c8520e03c0679b0e7bbbc1b
xhu6 <143360206+xhu6@users.noreply.github.com>
2023-08-28 16:25:36 +0100

    Added serviceable

--- car/car.py
+++ car/car.py
@@ -1,7 +1,8 @@
+from .serviceable import Serviceable
 from battery import Battery
 from engine import Engine
 
-class Car:
+class Car(Serviceable):
     def __init__(self, engine: Engine, battery: Battery):
         self._engine = engine
         self._battery = battery
