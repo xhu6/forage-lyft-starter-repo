7ab7c80debf5293abb57eb4356e52b6dcb93adb5
xhu6 <143360206+xhu6@users.noreply.github.com>
2023-08-28 21:52:54 +0100

    Upgraded Spindler and updated relevant tests

--- battery/spindler.py
+++ battery/spindler.py
@@ -7,5 +7,5 @@ class Spindler(Battery):
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
         return service_threshold_date < self.current_date
--- test/test_battery.py
+++ test/test_battery.py
@@ -5,14 +5,14 @@ from battery import Spindler, Nubbin
 class TestSpindler(unittest.TestCase):
     def test_should_be_serviced(self) -> None:
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
 
         battery = Spindler(last_service_date, today)
         self.assertTrue(battery.needs_service())
 
     def test_should_not_be_serviced(self) -> None:
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 1)
+        last_service_date = today.replace(year=today.year - 2)
 
         battery = Spindler(last_service_date, today)
         self.assertFalse(battery.needs_service())
