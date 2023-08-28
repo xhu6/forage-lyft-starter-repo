7b8d9f3ee6c69f36794269c2af397c199d9b69a8
xhu6 <143360206+xhu6@users.noreply.github.com>
2023-08-28 22:21:00 +0100

    Indicate private attributes for Battery

--- battery/spindler.py
+++ battery/spindler.py
@@ -3,9 +3,9 @@ from datetime import date
 
 class Spindler(Battery):
     def __init__(self, last_service_date: date, current_date: date) -> None:
-        self.last_service_date = last_service_date
-        self.current_date = current_date
+        self._last_service_date = last_service_date
+        self._current_date = current_date
 
     def needs_service(self) -> bool:
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
-        return service_threshold_date < self.current_date
+        service_threshold_date = self._last_service_date.replace(year=self._last_service_date.year + 3)
+        return service_threshold_date < self._current_date
