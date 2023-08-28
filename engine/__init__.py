f44fccf4670cd183a3e4dedbef180e5e687f1cbf
xhu6 <143360206+xhu6@users.noreply.github.com>
2023-08-28 21:43:12 +0100

    Fixed relative imports and updated gitignore for VS Code

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,6 @@
 from .engine import Engine
-from capulet_engine import CapuletEngine
-from sternman_engine import SternmanEngine
-from willoughby_engine import WilloughbyEngine
+from .capulet_engine import CapuletEngine
+from .sternman_engine import SternmanEngine
+from .willoughby_engine import WilloughbyEngine
 
 __all__ = ["Engine", "CapuletEngine", "SternmanEngine", "WilloughbyEngine"]
